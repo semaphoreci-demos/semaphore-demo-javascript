5b22190abbbee3c2bcc72b142fe39627ecd7a6fc
loginwashere <login.was.here@gmail.com>
2019-03-05 13:18:19 +0200

    Add client deploy to gh pages

--- src/server/src/app.module.ts
+++ src/server/src/app.module.ts
@@ -1,24 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApiModule } from './api/api.module';
-import config from './config';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: config.DATABASE_HOST,
-      port: parseInt(config.DATABASE_PORT, 10),
-      username: config.DATABASE_USER,
-      password: config.DATABASE_PASSWORD,
-      database: config.DATABASE_DBNAME,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: false,
-      migrationsRun: true,
-      migrations: ['migration/*.js'],
-      cli: {
-        migrationsDir: 'migration',
-      },
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useExisting: ConfigService,
     }),
     ApiModule,
   ],
