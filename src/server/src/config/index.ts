3fe6c72f8c83e1ae0fb3ec6f5bca1b1e4fdb8412
loginwashere <login.was.here@gmail.com>
2019-03-05 13:41:34 +0200

    Remove not used old config file

--- src/server/src/config/index.ts
+++
@@ -1,7 +0,0 @@
-import * as dotenv from 'dotenv';
-import * as fs from 'fs';
-import * as path from 'path';
-
-const config = dotenv.parse(fs.readFileSync(path.resolve(__dirname, '..', '..', '.env')));
-
-export default config;
