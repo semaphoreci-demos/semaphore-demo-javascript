d31f50512276599efd419a3be32ae24e39e416ca
Krisztian <krisztian.mihaly@risingstack.com>
2022-09-05 16:04:38 +0200

    tryout

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -4,9 +4,11 @@ import './index.css';
 import Root from './components/Root';
 import * as serviceWorker from './serviceWorker';
 
-import configureStore, {history} from './store';
+import configureStore, { history } from './store';
 
-const {store} = configureStore();
+const { store } = configureStore();
+
+console.log('test')
 
 ReactDOM.render(
   <Root
